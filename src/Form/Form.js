7a69e2dbfd9f7b1e27d4fbbc2dd3faf28e160287
Al Camara <Alhousseinec@gmail.com>
2023-02-22 16:51:20 -0600

    added state and update functions

--- src/Form/Form.js
+++ src/Form/Form.js
@@ -1,13 +1,48 @@
 import * as React from "react";
+import { useEffect, useState } from "react";
 import Box from "@mui/material/Box";
 import TextField from "@mui/material/TextField";
 import Button from "@mui/material/Button";
 
 export default function Form() {
+  const [sku, setSku] = useState("");
+  const [description, setDescription] = useState("");
+  const [price, setPrice] = useState("");
+
+  useEffect(() => {
+    console.log("refresh");
+  }, []);
+
   let style = {
     display: "flex",
     justifyContent: "space-between",
     marginBottom: "50px",
+  };
+
+  const updateDescription = ({ target }) => {
+    setDescription(target.value);
+  };
+
+  const updateSku = ({ target }) => {
+    setSku(target.value);
+  };
+
+  const updatePrice = ({ target }) => {
+    setPrice(target.value);
+  };
+
+  const submitButton = () => {
+    const data = {
+      sku: parseInt(sku),
+      descriptions: description,
+      price: parseFloat(price),
+    };
+
+    setDescription("");
+    setPrice("");
+    setSku("");
+
+    console.log(data);
   };
 
   return (
@@ -31,18 +66,24 @@ export default function Form() {
       </h2>
       <div style={style}>
         <TextField
+          onChange={updateSku}
+          value={sku}
           id="outlined-multiline-flexible"
           label="SKU"
           multiline
           maxRows={4}
         />
         <TextField
+          onChange={updateDescription}
+          value={description}
           id="outlined-multiline-flexible"
           label="Description"
           multiline
           maxRows={4}
         />
         <TextField
+          onChange={updatePrice}
+          value={price}
           id="outlined-multiline-flexible"
           label="Price"
           multiline
@@ -55,7 +96,11 @@ export default function Form() {
           justifyContent: "flex-end",
         }}
       >
-        <Button style={{ fontSize: "1.4rem" }} variant="contained">
+        <Button
+          onClick={submitButton}
+          style={{ fontSize: "1.4rem" }}
+          variant="contained"
+        >
           Add
         </Button>
       </div>
