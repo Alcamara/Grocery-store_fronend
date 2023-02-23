b19c4eacc0dc2e89462bd84c5b440c97b430a4ba
Al Camara <Alhousseinec@gmail.com>
2023-02-23 17:48:47 -0600

    added dispatch to update button

--- src/ProductsView/ProductsView.js
+++ src/ProductsView/ProductsView.js
@@ -1,7 +1,8 @@
 import * as React from 'react';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { useSelector } from 'react-redux';
 import { useDispatch } from 'react-redux';
+// table
 import Table from '@mui/material/Table';
 import TableBody from '@mui/material/TableBody';
 import TableCell from '@mui/material/TableCell';
@@ -11,17 +12,50 @@ import TableRow from '@mui/material/TableRow';
 import Paper from '@mui/material/Paper';
 import { Stack } from '@mui/system';
 import { Button } from '@mui/material';
-
+//dialog
+import Dialog from '@mui/material/Dialog';
+import DialogActions from '@mui/material/DialogActions';
+import DialogContent from '@mui/material/DialogContent';
+import DialogContentText from '@mui/material/DialogContentText';
+import DialogTitle from '@mui/material/DialogTitle';
+//textfield
+import TextField from '@mui/material/TextField';
 
 
 export default function ProductView() {
+    const [open, setOpen] = useState(false)
+    const [sku, setSku] = useState('');
+    const [description, setDescription] = useState({description:''});
+    const [price, setPrice] = useState({price:''});
     const dispatch = useDispatch()
+
+
 
     useEffect(()=>{
         dispatch({type:"FETCH_PRODUCTS"})
     },[])
 
 const products = useSelector(store => store.products)
+
+const handleClickOpen = () =>{
+    setOpen(true)
+}
+
+const handleClose = () =>{
+    setOpen(false)
+}
+
+const updateDescription = ({ target }) => {
+    setDescription(target.value);
+  };
+
+  const updateSku = ({ target }) => {
+    setSku(target.value);
+  };
+
+  const updatePrice = ({ target }) => {
+    setPrice(target.value);
+  };
 
 const table = (
     <div style={{padding:"60px 0", width: '60%', margin: "0 auto"}}>
@@ -48,7 +82,65 @@ const table = (
               <TableCell align="right">{product.price}</TableCell>
               <TableCell align="right" > 
               <Stack spacing={2} direction="row">
-                <Button variant="contained" color='success'>Edit</Button>
+                <Button 
+                    variant="contained" 
+                    color='success'
+                    onClick={()=>{
+                        handleClickOpen()
+                        setSku(product.sku.toString())
+                        setDescription(product.description)
+                        setPrice(product.price.toString())
+                    }}
+                >
+                    Edit
+                </Button>
+                <Dialog open={open} onClose={handleClose}>
+                    <DialogTitle>Edit Form</DialogTitle>
+                    <DialogContent  >
+                        <TextField 
+                            fullWidth
+                            value={sku}
+                            onChange={updateSku} 
+                            label='Sku'
+                        />
+                    </DialogContent>
+                    <DialogContent  >
+                        <TextField 
+                            fullWidth 
+                            value={description}
+                            onChange={updateDescription}  
+                            label='Description'
+                        />
+                    </DialogContent>
+                    <DialogContent  >
+                        <TextField 
+                            fullWidth 
+                            value={price}
+                            onChange={updatePrice}  
+                            label='Price'/>
+                    </DialogContent>
+                    <DialogActions>
+                        <Button
+                            onClick={()=>{
+                                dispatch({
+                                    type:'UPDATE_PRODUCT',
+                                    payload: {
+                                        id: product.id,
+                                        sku: parseInt(sku),
+                                        description: description,
+                                        price: parseFloat(price)
+                                    }
+                                })
+
+                                handleClose()
+                            }}
+                        
+                        >
+                            Update
+                        </Button>
+                        <Button onClick={handleClose}>Canel</Button>
+                    </DialogActions>
+                </Dialog>
                 <Button variant="outlined" color='error'>Delete</Button>
               </Stack>
               </TableCell>
--- src/redux/sagas/products.saga.js
+++ src/redux/sagas/products.saga.js
@@ -1,5 +1,6 @@
 import { put, takeLatest } from 'redux-saga/effects';
 import axios from "axios";
+import { act } from 'react-dom/test-utils';
 
 function* fetchProducts() {
   try {
@@ -11,8 +12,23 @@ function* fetchProducts() {
   }
 }
 
+
+function* updateProduct(action) {
+  console.log();
+  try {
+    const resp = yield axios.put("http://127.0.0.1:8000/product/"+action.payload.id, action.payload);
+    console.log(resp);
+    yield put({type: "FETCH_PRODUCTS"})
+  } catch (err) {
+    console.log(`postForm err: ${err}`);
+  }
+}
+
+
+
 function* productsSaga() {
   yield takeLatest("FETCH_PRODUCTS", fetchProducts);
+  yield takeLatest("UPDATE_PRODUCT", updateProduct);
 }
 
 export default productsSaga;
